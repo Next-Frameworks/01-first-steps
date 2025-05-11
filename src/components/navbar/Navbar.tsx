25885d1a833562385317d85aa6f0a561dbdef6e6
Luis Angel <angelfgdeveloper@gmail.com>
2025-05-11 15:19:35 -0600

    Server component para carga de back

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,16 @@
 
+// const temporalAsync = () => {
+//   return new Promise( (resolve) => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, 2000);
+//   });
+// }
 
-export const Navbar = () => {
+export const Navbar = /*async*/ () => {
+
+  // await temporalAsync();
+
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <span>Home</span>
