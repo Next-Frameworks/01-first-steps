28cda55719bfcb20d81740e8cba669d66cb883c7
Luis Angel <angelfgdeveloper@gmail.com>
2025-05-11 15:33:48 -0600

    Next/Link navbar dinamico server component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,19 +7,41 @@
 //   });
 // }
 
+// Ya no hace con Link un refresh como con el <a></a>
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+
+const navItems = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+];
+
 export const Navbar = /*async*/ () => {
 
   // await temporalAsync();
 
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={'/'} className='flex items-center'>
+      <HomeIcon className='mr-2'/>
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
 
-      <a className="mr-2" href="/about">About</a>
-      <a className="mr-2" href="/pricing">Princing</a>
-      <a className="mr-2" href="/contact">Contact</a>
+      {
+        navItems.map(navItem => (
+          <Link 
+          className="mr-2"
+          key={ navItem.path }
+          href={ navItem.path }>{ navItem.text }</Link>
+        ))
+      }
+
+      {/* <Link className="mr-2" href="/about">About</Link>
+      <Link className="mr-2" href="/pricing">Princing</Link>
+      <Link className="mr-2" href="/contact">Contact</Link> */}
     </nav>
   );
 }
