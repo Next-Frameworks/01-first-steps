5a5bdd8c8e5be1c118fc31c8198393b365922c59
Luis Angel <angelfgdeveloper@gmail.com>
2025-05-11 21:54:36 -0600

    Generando images con docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+// Añadir en documentacion
+// https://nextjs.org/docs/pages/getting-started/deploying#docker
+
+// Ejecutar dockerfile: 
+// docker build -t nextjs-first-steps .
+
+// Ejecutar para crear contentedor: 
+// docker container run -dp 3000:3000 nextjs-first-steps
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "standalone",
 };
 
 export default nextConfig;
